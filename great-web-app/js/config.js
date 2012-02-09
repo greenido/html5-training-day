e3ea391fb282a3337382aa4f055b1fa7d99258db
Ido Green <greenido@gmail.com>
2012-02-09 18:41:33 +0200

    2 more slides for talks this month

---
+++ great-web-app/js/config.js
@@ -0,0 +1,36 @@
+window.slidesConfig = window.slidesConfig || {
+  // Slide settings
+  settings : {
+    useBuilds: true,
+    hashtag: '#HTML5TLV',
+    showHiddenSlides: false
+  },
+
+  info: {
+    // Presentation Name
+    subtitle: 'Designing &amp; Building',
+    title: 'Great Web Applications',
+
+
+    // Personal info
+    name: 'Ido Green',
+    pic: 'https://lh4.googleusercontent.com/-YpY1A_LkwH8/AAAAAAAAAAI/AAAAAAAAGPY/hpy-JaS2PtQ/photo.jpg?sz=200',
+    gplus: 'https://plus.ly/greenido',
+    twitter: 'greenido',
+    blog: 'http://greenido.wordpress.com',
+
+    // Google Analytics Token
+    ga_account: 'UA-27750723-1',
+
+    // Links
+    slides: 'http://ido-green.appspot.com/',
+    feedback: 'https://plus.ly/greenido',
+    location: 'Tel Aviv, Israel',
+
+    // Presentation Timer
+    date: new Date('Mar 1 2012 10:00'),
+    logo: './images/html5_logo_256.png',
+    minutes: 50,
+    warnAt: 10
+  }
+};
---
+++ mobile-web-app/js/config.js
@@ -0,0 +1,23 @@
+window.slidesConfig = window.slidesConfig || {
+  // Slide settings
+  settings : {
+    useBuilds: true,
+    useGDDBranding: false,
+    includeWebRTC: true,
+    hashtag: '#HTML5TLV'
+  },
+  info: {
+    // Personal info
+    name: 'Ido Green',
+    pic: 'https://lh4.googleusercontent.com/-YpY1A_LkwH8/AAAAAAAAAAI/AAAAAAAAGPY/hpy-JaS2PtQ/photo.jpg?sz=200',
+    gplus: 'https://plus.google.com/101709676645745930035/',
+    twitter: 'greenido',
+    blog: 'http://greenido.wordpress.com/',
+
+    // GDD info
+    location: 'Tel Aviv, Israel',
+    date: new Date('March 4 2011'),
+    logo: 'images/chrome-israel-logo-tiny.png'
+    
+  }
+};
